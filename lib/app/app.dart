e509f637eb810d2409b598960e3021538f884687
Nivethiga07 <iamnive07@gmail.com>
2026-09-08 12:35:20 +0530

    Add missing GoogleFonts import

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../core/theme/pos_theme_provider.dart';
 import '../features/auth/presentation/providers/auth_network_provider.dart';
